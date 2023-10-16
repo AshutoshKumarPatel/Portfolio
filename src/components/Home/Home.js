8667a8eaa80d17c8c94baaebdbf42ef61f2c6da5
Ashutosh <ashutosh.kumar.patel.2003@gmail.com>
2023-10-16 23:26:32 +0530

    updated pages and title

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 
+
 function Home() {
+  useEffect(() => {
+    document.title = 'Task 6';
+  }, []);
   return (
     <section>
       <Container fluid className="home-section" id="home">
